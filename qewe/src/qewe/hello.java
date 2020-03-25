c3f49cc62ec4cfebfd3a85344b882c22e1f020f4
fal975975 <1955077761@qq.com>
2020-03-25 16:46:37 +0800

    新增文件

---
+++ qewe/src/qewe/hello.java
@@ -0,0 +1,18 @@
+package qewe;
+/**@文件名: hello.java
+ * @类功能说明: 
+ * @作者: WuMengFan
+ * @Email: 1955077761@qq.com
+ * @日期: 2020年3月25日下午4:24:56
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: WuMengFan</li> 
+ * 	 <li>日期: 2020年3月25日下午4:24:56</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class hello {
+public static void main(String[] args) {
+	System.out.println("HELLO WORD");
+}
+}
